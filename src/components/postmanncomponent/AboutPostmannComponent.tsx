b3f905f229196f9ee00043076542d77da78a5969
Shatadip Majumder <shatadip.majumder@gmail.com>
2024-05-09 20:49:22 +0530

    WIP: added Disclaimer in AboutPostmannComponent modal element.

--- src/components/postmanncomponent/AboutPostmannComponent.tsx
+++ src/components/postmanncomponent/AboutPostmannComponent.tsx
@@ -1,7 +1,18 @@
+import './AboutPostmannComponent.css';
 
 const AboutPostmannComponent = () => {
   return (
-    <div>AboutPostmannComponent</div>
+    <>
+    <div className='apc-label'>AboutPostmannComponent</div>
+    <p className='apc-disclaimer'>Disclaimer</p>
+    <ul className='apc-ul'>
+      <li>Postmann is an open-source REST Client under MIT License</li>
+      <li>Postmann is not an official Chrome Extension of Postman</li>
+      <li>Postmann is not affiliated with Postman in any way</li>
+      <li>None of your requests are monitored or tracked</li>
+      <li>This Chrome Extension does not come with any guarantee of any sort</li>
+    </ul>
+    </>
   )
 }
 
