f8436a0d4785ace202200b787b85ae0becdf13f6
shatadip <shatadip.majumder@gmail.com>
2024-05-21 23:58:55 +0530

    Changed: UI Styles (Table row header) in HeadersMRHComponent and VariablesComponent.

--- src/components/mrhcomponent/HeadersMRHComponent.tsx
+++ src/components/mrhcomponent/HeadersMRHComponent.tsx
@@ -1,8 +1,8 @@
 //HeadersMRHComponent.tsx
 
-import React, { useState, useEffect, useRef } from 'react';
-import { Trash, Clipboard } from 'react-bootstrap-icons';
-import './HeadersMRHComponent.css';
+import React, { useState, useEffect, useRef } from "react";
+import { Trash, Clipboard } from "react-bootstrap-icons";
+import "./HeadersMRHComponent.css";
 
 interface header {
   name: string;
@@ -11,9 +11,11 @@ interface header {
 
 const HeadersMRHComponent: React.FC = () => {
   const [headersMRH, setHeadersMRH] = useState<header[]>([]);
-  const [restoreButtonText, setRestoreButtonText] = useState<string>('Restore Defaults');
-  const [restoreButtonBGColor, setRestoreButtonBGColor] = useState<string>('#F9F9F9');
-  const [btnTextColor, setBtnTextColor] = useState<string>('buttontext');
+  const [restoreButtonText, setRestoreButtonText] =
+    useState<string>("Restore Defaults");
+  const [restoreButtonBGColor, setRestoreButtonBGColor] =
+    useState<string>("#F9F9F9");
+  const [btnTextColor, setBtnTextColor] = useState<string>("buttontext");
   const [isRestoring, setIsRestoring] = useState(false);
   const [justMounted, setJustMounted] = useState(true);
 
@@ -21,11 +23,11 @@ const HeadersMRHComponent: React.FC = () => {
   const valueRefs = useRef<HTMLDivElement[]>([]); // Ref for value fields
 
   const defaultHeaders = [
-    { name: 'Content-Type', value: 'application/json' },
-    { name: 'Cache-Control', value: 'no-cache' },
-    { name: 'Accept', value: '*/*' },
-    { name: 'Accept-Encoding', value: 'gzip, deflate, br' },
-    { name: 'Connection', value: 'keep-alive' },
+    { name: "Content-Type", value: "application/json" },
+    { name: "Cache-Control", value: "no-cache" },
+    { name: "Accept", value: "*/*" },
+    { name: "Accept-Encoding", value: "gzip, deflate, br" },
+    { name: "Connection", value: "keep-alive" },
   ];
 
   // use useEffect to remove empty headers (both header name and value are empty) leaving just mounted headers to avoid racing conditions
@@ -41,12 +43,14 @@ const HeadersMRHComponent: React.FC = () => {
   // Only run this useEffect after the initial render
   useEffect(() => {
     if (!justMounted) {
-      setHeadersMRH(headersMRH.filter(header => header.name !== '' || header.value !== ''));
+      setHeadersMRH(
+        headersMRH.filter((header) => header.name !== "" || header.value !== "")
+      );
     }
   }, [headersMRH, justMounted]);
 
   useEffect(() => {
-    const storedHeadersMRH = localStorage.getItem('postmannHeadersMRH');
+    const storedHeadersMRH = localStorage.getItem("postmannHeadersMRH");
     if (storedHeadersMRH) {
       setHeadersMRH(JSON.parse(storedHeadersMRH));
     } else {
@@ -56,7 +60,7 @@ const HeadersMRHComponent: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('postmannHeadersMRH', JSON.stringify(headersMRH));
+    localStorage.setItem("postmannHeadersMRH", JSON.stringify(headersMRH));
   }, [headersMRH]);
 
   useEffect(() => {
@@ -66,7 +70,7 @@ const HeadersMRHComponent: React.FC = () => {
 
   const addHeader = () => {
     // setJustMounted(true);
-    const newHeadersMRH = [...headersMRH, { name: '', value: '' }];
+    const newHeadersMRH = [...headersMRH, { name: "", value: "" }];
     setHeadersMRH(newHeadersMRH);
     // Focus on the last added header's name field
     const lastIndex = newHeadersMRH.length - 1;
@@ -77,18 +81,18 @@ const HeadersMRHComponent: React.FC = () => {
 
   const restoreDefaultHeaders = () => {
     setIsRestoring(true);
-    setRestoreButtonText('Restoring...');
-    setRestoreButtonBGColor('#0b5f03'); // Change to green
-    setBtnTextColor('#b2ff00');
+    setRestoreButtonText("Restoring...");
+    setRestoreButtonBGColor("#0b5f03"); // Change to green
+    setBtnTextColor("#b2ff00");
     setTimeout(() => {
-      setBtnTextColor('#ffffff');
-      setRestoreButtonText('Restored Successfully');
+      setBtnTextColor("#ffffff");
+      setRestoreButtonText("Restored Successfully");
     }, 1000);
     setTimeout(() => {
       setHeadersMRH(defaultHeaders);
-      setRestoreButtonText('Restore Defaults');
-      setRestoreButtonBGColor('#F9F9F9'); // Change back to the original color
-      setBtnTextColor('buttontext');
+      setRestoreButtonText("Restore Defaults");
+      setRestoreButtonBGColor("#F9F9F9"); // Change back to the original color
+      setBtnTextColor("buttontext");
       setIsRestoring(false);
     }, 1750);
   };
@@ -104,20 +108,25 @@ const HeadersMRHComponent: React.FC = () => {
     // Check if the valueRefs array has an element at the given index
     if (valueRefs.current[index]) {
       // Get the text content of the value field
-      const valueToCopy = valueRefs.current[index].textContent || '';
+      const valueToCopy = valueRefs.current[index].textContent || "";
 
       // Use the Clipboard API to copy the value
-      navigator.clipboard.writeText(valueToCopy).then(() => {
-        console.log('Header value copied to clipboard');
-      }).catch(err => {
-        console.error('Failed to copy header value: ', err);
-      });
+      navigator.clipboard
+        .writeText(valueToCopy)
+        .then(() => {
+          console.log("Header value copied to clipboard");
+        })
+        .catch((err) => {
+          console.error("Failed to copy header value: ", err);
+        });
     }
   };
 
-
-
-  const handleInputChange = (index: number, key: keyof header, value: string) => {
+  const handleInputChange = (
+    index: number,
+    key: keyof header,
+    value: string
+  ) => {
     const updatedHeadersMRH = [...headersMRH];
     updatedHeadersMRH[index][key] = value;
     setHeadersMRH(updatedHeadersMRH);
@@ -128,21 +137,21 @@ const HeadersMRHComponent: React.FC = () => {
     key: keyof header,
     e: React.KeyboardEvent<HTMLDivElement>
   ) => {
-    if (e.key === 'Tab') {
+    if (e.key === "Tab") {
       e.preventDefault();
       if (e.shiftKey) {
-        if (key === 'value') {
+        if (key === "value") {
           nameRefs.current[index]?.focus();
         } else if (index > 0) {
           valueRefs.current[index - 1]?.focus();
         }
       } else {
-        if (key === 'name') {
+        if (key === "name") {
           valueRefs.current[index]?.focus();
         } else if (index < headersMRH.length - 1) {
           nameRefs.current[index + 1]?.focus();
         } else if (index === headersMRH.length - 1) {
-        // setJustMounted(true);
+          // setJustMounted(true);
           addHeader();
         }
       }
@@ -151,12 +160,30 @@ const HeadersMRHComponent: React.FC = () => {
 
   return (
     <>
-      <div className='mrh-label'>Headers are auto-saved, add/change them as needed.</div>
-      <div className="mrh-label flex-icon"><img src="click-outside-2.png" alt="Click Outside" style={{ width: '24px' }} /> Click outside to save</div>
-      <div className='mrh-label flex-icon'>¯\_(ツ)_/¯ Don't worry if you screw up, you can always restore the default headers.</div>
-      <div className='mrh-label'>You currently have <span className='squared-number'>{headersMRH.length}</span> headers{headersMRH.length == 0 ? ', add one by clicking on \'Add Header\' button' : ''}</div>
+      <div className="mrh-label">
+        Headers are auto-saved, add/change them as needed.
+      </div>
+      <div className="mrh-label flex-icon">
+        <img
+          src="click-outside-2.png"
+          alt="Click Outside"
+          style={{ width: "24px" }}
+        />{" "}
+        Click outside to save
+      </div>
+      <div className="mrh-label flex-icon">
+        ¯\_(ツ)_/¯ Don't worry if you screw up, you can always restore the
+        default headers.
+      </div>
+      <div className="mrh-label">
+        You currently have{" "}
+        <span className="squared-number">{headersMRH.length}</span> headers
+        {headersMRH.length == 0
+          ? ", add one by clicking on 'Add Header' button"
+          : ""}
+      </div>
       <div className="table-container">
-        <div className="table-row header">
+        <div className="table-row header goldenrod">
           <div className="table-cell var-name">Header</div>
           <div className="table-cell">Value</div>
           <div className="table-cell">Options</div>
@@ -164,37 +191,72 @@ const HeadersMRHComponent: React.FC = () => {
         {headersMRH.map((header, index) => (
           <div className="table-row" key={index}>
             <div
-              ref={(element) => (nameRefs.current[index] = element as HTMLDivElement)}
+              ref={(element) =>
+                (nameRefs.current[index] = element as HTMLDivElement)
+              }
               className="table-cell var-name"
               contentEditable
-              onBlur={(e) => handleInputChange(index, 'name', e.currentTarget.textContent || '')}
-              onKeyDown={(e) => handleKeyPress(index, 'name', e)}
+              onBlur={(e) =>
+                handleInputChange(
+                  index,
+                  "name",
+                  e.currentTarget.textContent || ""
+                )
+              }
+              onKeyDown={(e) => handleKeyPress(index, "name", e)}
             >
               {header.name}
             </div>
             <div
-              ref={(element) => (valueRefs.current[index] = element as HTMLDivElement)} // Ref for value field
+              ref={(element) =>
+                (valueRefs.current[index] = element as HTMLDivElement)
+              } // Ref for value field
               className="table-cell"
               contentEditable
-              onBlur={(e) => handleInputChange(index, 'value', e.currentTarget.textContent || '')}
-              onKeyDown={(e) => handleKeyPress(index, 'value', e)}
+              onBlur={(e) =>
+                handleInputChange(
+                  index,
+                  "value",
+                  e.currentTarget.textContent || ""
+                )
+              }
+              onKeyDown={(e) => handleKeyPress(index, "value", e)}
             >
               {header.value}
             </div>
             <div className="table-cell buttons-div">
-              <div style={{ display: 'flex', gap: '0.45rem', border: 'none', alignItems: 'center', justifyContent: 'center' }}>
-                <Clipboard className='icon-copy-header-value' data-tooltip='Copy Value' onClick={() => copyHeaderVal(index)} />
-                <Trash className='icon-delete-header' data-tooltip='Delete Header' onClick={() => deleteHeader(index)} />
+              <div
+                style={{
+                  display: "flex",
+                  gap: "0.45rem",
+                  border: "none",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Clipboard
+                  className="icon-copy-header-value"
+                  data-tooltip="Copy Value"
+                  onClick={() => copyHeaderVal(index)}
+                />
+                <Trash
+                  className="icon-delete-header"
+                  data-tooltip="Delete Header"
+                  onClick={() => deleteHeader(index)}
+                />
               </div>
             </div>
           </div>
         ))}
       </div>
-      <div className='btnContainerMRH'>
+      <div className="btnContainerMRH">
         <button onClick={addHeader}>Add Header</button>
         <button
           onClick={restoreDefaultHeaders}
-          style={{ backgroundColor: `${restoreButtonBGColor}`, color: `${btnTextColor}` }}
+          style={{
+            backgroundColor: `${restoreButtonBGColor}`,
+            color: `${btnTextColor}`,
+          }}
           disabled={isRestoring}
         >
           {restoreButtonText}
--- src/components/variablescomponent/VariablesComponent.tsx
+++ src/components/variablescomponent/VariablesComponent.tsx
@@ -1,8 +1,8 @@
 // VariablesComponent.tsx
 
-import React, { useState, useEffect, useRef } from 'react';
-import { Trash, Clipboard, ExclamationTriangle } from 'react-bootstrap-icons';
-import './VariablesComponent.css';
+import React, { useState, useEffect, useRef } from "react";
+import { Trash, Clipboard, ExclamationTriangle } from "react-bootstrap-icons";
+import "./VariablesComponent.css";
 
 interface Variable {
   name: string;
@@ -11,9 +11,9 @@ interface Variable {
 
 const VariablesComponent: React.FC = () => {
   const [variables, setVariables] = useState<Variable[]>([]);
-  const [warnStyle, setWarnStyle] = useState<String>('none');
+  const [warnStyle, setWarnStyle] = useState<String>("none");
   const [isShaking, setIsShaking] = useState(false);
-  const [justMounted, setJustMounted] = useState(true); // New state to track if component just mounted
+  const [justMounted, setJustMounted] = useState(false); // New state to track if component just mounted
   const maxNumOfVars = 42;
   // [FOR TEST PURPOSES]
   // const [displayMsgText, setDisplayMsgText] = useState<string>('');
@@ -21,7 +21,7 @@ const VariablesComponent: React.FC = () => {
   const valueRefs = useRef<HTMLDivElement[]>([]); // Ref for value fields
 
   useEffect(() => {
-    const storedVariables = localStorage.getItem('postmannVars');
+    const storedVariables = localStorage.getItem("postmannVars");
     if (storedVariables) {
       setVariables(JSON.parse(storedVariables));
     }
@@ -30,7 +30,7 @@ const VariablesComponent: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('postmannVars', JSON.stringify(variables));
+    localStorage.setItem("postmannVars", JSON.stringify(variables));
   }, [variables]);
 
   useEffect(() => {
@@ -57,7 +57,7 @@ const VariablesComponent: React.FC = () => {
     if (!justMounted && variables.length > 0) {
       let emptyVars = 0;
       variables.forEach((variable, index) => {
-        if (variable.name === '' && variable.value === '') {
+        if (variable.name == "" && variable.value == "") {
           emptyVars++;
           deleteVariable(index);
         }
@@ -68,11 +68,10 @@ const VariablesComponent: React.FC = () => {
     }
   }, [variables, justMounted]);
 
-
   const addVariable = () => {
     if (variables.length < maxNumOfVars) {
       setJustMounted(true);
-      const newVariables = [...variables, { name: '', value: '' }];
+      const newVariables = [...variables, { name: "", value: "" }];
       setVariables(newVariables);
       // Focus on the last added variable's name field
       const lastIndex = newVariables.length - 1;
@@ -81,7 +80,7 @@ const VariablesComponent: React.FC = () => {
       }, 0);
     } else {
       // Set a warning message or disable the "Add Variable" button
-      setWarnStyle('block');
+      setWarnStyle("block");
       setIsShaking(true);
 
       // Remove the "shaker" class after 2 seconds
@@ -93,14 +92,16 @@ const VariablesComponent: React.FC = () => {
 
   const deleteVariable = (index: number) => {
     setVariables(variables.filter((_, i) => i !== index));
-    setWarnStyle('none');
+    setWarnStyle("none");
   };
   const deleteAllVariables = () => {
-    const isConfirmed = window.confirm('Are you sure you want to delete all variables?');
+    const isConfirmed = window.confirm(
+      "Are you sure you want to delete all variables?"
+    );
 
     if (isConfirmed) {
       setVariables([]);
-      setWarnStyle('none');
+      setWarnStyle("none");
     }
   };
   const copyVarVal = (index: number) => {
@@ -110,38 +111,51 @@ const VariablesComponent: React.FC = () => {
     // Check if the valueRefs array has an element at the given index
     if (valueRefs.current[index]) {
       // Get the text content of the value field
-      const valueToCopy = valueRefs.current[index].textContent || '';
+      const valueToCopy = valueRefs.current[index].textContent || "";
 
       // Use the Clipboard API to copy the value
-      navigator.clipboard.writeText(valueToCopy).then(() => {
-        console.log('Variable value copied to clipboard');
-      }).catch(err => {
-        console.error('Failed to copy variable value: ', err);
-      });
+      navigator.clipboard
+        .writeText(valueToCopy)
+        .then(() => {
+          console.log("Variable value copied to clipboard");
+        })
+        .catch((err) => {
+          console.error("Failed to copy variable value: ", err);
+        });
     }
   };
 
-
-  const handleInputChange = (index: number, key: keyof Variable, value: string) => {
+  const handleInputChange = (
+    index: number,
+    key: keyof Variable,
+    value: string
+  ) => {
     let updatedVariables = [...variables];
     updatedVariables[index][key] = value;
     setVariables(updatedVariables);
   };
 
-
-  const handleKeyPress = (index: number, key: keyof Variable, e: React.KeyboardEvent<HTMLDivElement>) => {
-    if (e.key === 'Tab') {
+  const handleKeyPress = (
+    index: number,
+    key: keyof Variable,
+    e: React.KeyboardEvent<HTMLDivElement>
+  ) => {
+    if (e.key === "Tab") {
       e.preventDefault();
-      if (e.shiftKey) { // If Shift+Tab is pressed
-        if (key === 'name') { // If currently focused on a name field
+      if (e.shiftKey) {
+        // If Shift+Tab is pressed
+        if (key === "name") {
+          // If currently focused on a name field
           // Delay the focus change after the blur event
           setTimeout(() => {
-            if (index > 0) { // If not the first variable
+            if (index > 0) {
+              // If not the first variable
               // Focus on the previous variable's value field
               valueRefs.current[index - 1]?.focus();
             }
           }, 10);
-        } else if (key === 'value') { // If currently focused on a value field
+        } else if (key === "value") {
+          // If currently focused on a value field
           // Delay the focus change after the blur event
           setTimeout(() => {
             // Focus on the current variable's name field
@@ -149,23 +163,29 @@ const VariablesComponent: React.FC = () => {
           }, 10);
         }
       } else {
-        if (key === 'name') {
+        if (key === "name") {
           // Focus on the current variable's value field
           valueRefs.current[index]?.focus();
-        } else if (key === 'value' && index === variables.length - 1) {
+        } else if (key === "value" && index === variables.length - 1) {
           // add a new variable if the last variable's value field is focused
           addVariable();
-        } else if (index < variables.length - 1) { // If currently focused on a value field and not the last variable
+        } else if (index < variables.length - 1) {
+          // If currently focused on a value field and not the last variable
           // Focus on the next variable's name field
           nameRefs.current[index + 1]?.focus();
         }
       }
     }
     // Remove whitespace and special characters when Enter or Shift+Enter is pressed
-    if ((e.key == 'Enter' || (e.shiftKey && e.key == 'Enter') || e.key == 'Tab') && key == 'name') {
+    if (
+      (e.key == "Enter" ||
+        (e.shiftKey && e.key == "Enter") ||
+        e.key == "Tab") &&
+      key == "name"
+    ) {
       e.preventDefault(); // Prevent default behavior
-      let sanitizedName = (e.currentTarget.textContent || '').trim(); // Trim the variable name
-      sanitizedName = sanitizedName.replace(/[^\w-]/g, ''); // Remove whitespace and special characters
+      let sanitizedName = (e.currentTarget.textContent || "").trim(); // Trim the variable name
+      sanitizedName = sanitizedName.replace(/[^\w-]/g, ""); // Remove whitespace and special characters
 
       handleInputChange(index, key, sanitizedName);
       // Update the display message [FOR TEST PURPOSES]
@@ -179,11 +199,13 @@ const VariablesComponent: React.FC = () => {
   };
 
   const handleNameFieldBlur = (index: number, value: string) => {
+    let sanitizedValue = value.trim().replace(/[^\w-]/g, ""); // Remove whitespace and special characters
 
-    let sanitizedValue = value.trim().replace(/[^\w-]/g, ''); // Remove whitespace and special characters
-
-    handleInputChange(index, 'name', sanitizedValue == '' ? '\n' : sanitizedValue); // \n seems to be a hack to prevent only special characters used as a variable name
-
+    handleInputChange(
+      index,
+      "name",
+      sanitizedValue == "" ? "\n" : sanitizedValue
+    ); // \n seems to be a hack to prevent only special characters used as a variable name
   };
 
   return (
@@ -194,18 +216,37 @@ const VariablesComponent: React.FC = () => {
         {displayMsgText}
       </div> 
   */}
-      <div className='vars-label'>Variables are auto-saved, use &#x7B;&#x7B;var_name&#x7D;&#x7D; in URL input or body</div>
+      <div className="vars-label">
+        Variables are auto-saved, use &#x7B;&#x7B;var_name&#x7D;&#x7D; in URL
+        input or body
+      </div>
       {variables.length > 0 && (
         <>
-          <div className="vars-label flex-icon"><img src="click-outside-2.png" alt="Click Outside" style={{ width: '24px' }} /> Click outside to save</div>
-          <div className="vars-label flex-icon" style={{ marginLeft: '6px' }}><img src="tab.png" alt="Tab" style={{ width: '17px' }} /> Hit Tab/Shift+tab to navigate</div>
+          <div className="vars-label flex-icon">
+            <img
+              src="click-outside-2.png"
+              alt="Click Outside"
+              style={{ width: "24px" }}
+            />{" "}
+            Click outside to save
+          </div>
+          <div className="vars-label flex-icon" style={{ marginLeft: "6px" }}>
+            <img src="tab.png" alt="Tab" style={{ width: "17px" }} /> Hit
+            Tab/Shift+tab to navigate
+          </div>
         </>
       )}
-      <div className='vars-label'>You currently have <span className='squared-number'>{variables.length}</span> {variables.length == 1 ? 'variable' : 'variables'}{variables.length == 0 ? ', create one by clicking on \'Add Variable\' button.' : ''}</div>
+      <div className="vars-label">
+        You currently have{" "}
+        <span className="squared-number">{variables.length}</span>{" "}
+        {variables.length == 1 ? "variable" : "variables"}
+        {variables.length == 0
+          ? ", create one by clicking on 'Add Variable' button."
+          : ""}
+      </div>
       <div className="table-container">
-
         {variables.length > 0 && (
-          <div className="table-row header">
+          <div className="table-row header goldenrod">
             <div className="table-cell var-name">Variable Name</div>
             <div className="table-cell">Value</div>
             <div className="table-cell">Options</div>
@@ -215,44 +256,84 @@ const VariablesComponent: React.FC = () => {
         {variables.map((variable, index) => (
           <div className="table-row" key={index}>
             <div
-              ref={(element) => (nameRefs.current[index] = element as HTMLDivElement)}
+              ref={(element) =>
+                (nameRefs.current[index] = element as HTMLDivElement)
+              }
               className="table-cell var-name"
               contentEditable
               // onBlur={(e) => handleInputChange(index, 'name', e.currentTarget.textContent || '')}
-              onBlur={(e) => handleNameFieldBlur(index, e.currentTarget.textContent || '')}
-              onKeyDown={(e) => handleKeyPress(index, 'name', e)}
+              onBlur={(e) =>
+                handleNameFieldBlur(index, e.currentTarget.textContent || "")
+              }
+              onKeyDown={(e) => handleKeyPress(index, "name", e)}
             >
               {variable.name}
             </div>
             <div
-              ref={(element) => (valueRefs.current[index] = element as HTMLDivElement)} // Ref for value field
+              ref={(element) =>
+                (valueRefs.current[index] = element as HTMLDivElement)
+              } // Ref for value field
               className="table-cell"
-              style={{ whiteSpace: 'pre-wrap' }}
+              style={{ whiteSpace: "pre-wrap" }}
               contentEditable
-              onBlur={(e) => handleInputChange(index, 'value', e.currentTarget.textContent || '')}
-              onKeyDown={(e) => handleKeyPress(index, 'value', e)}
+              onBlur={(e) =>
+                handleInputChange(
+                  index,
+                  "value",
+                  e.currentTarget.textContent || ""
+                )
+              }
+              onKeyDown={(e) => handleKeyPress(index, "value", e)}
             >
               {variable.value}
             </div>
             <div className="table-cell buttons-div">
-              <div style={{ display: 'flex', gap: '0.45rem', alignItems: 'center', justifyContent: 'center' }}>
-                <Clipboard className='icon-copy-variable-value' data-tooltip='Copy Value' onClick={() => copyVarVal(index)} />
-                <Trash className='icon-delete-variable' data-tooltip='Delete Variable' onClick={() => deleteVariable(index)} />
+              <div
+                style={{
+                  display: "flex",
+                  gap: "0.45rem",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Clipboard
+                  className="icon-copy-variable-value"
+                  data-tooltip="Copy Value"
+                  onClick={() => copyVarVal(index)}
+                />
+                <Trash
+                  className="icon-delete-variable"
+                  data-tooltip="Delete Variable"
+                  onClick={() => deleteVariable(index)}
+                />
               </div>
             </div>
           </div>
         ))}
       </div>
       <div className="buttonContainerVars">
-        <button onClick={addVariable} id="btnAddVar" className={isShaking ? 'shake' : ''}>Add Variable</button>
+        <button
+          onClick={addVariable}
+          id="btnAddVar"
+          className={isShaking ? "shake" : ""}
+        >
+          Add Variable
+        </button>
         {variables.length > 0 && (
-          <button onClick={deleteAllVariables} id="btnDelAllVars"><ExclamationTriangle />Delete All Variables</button>
+          <button onClick={deleteAllVariables} id="btnDelAllVars">
+            <ExclamationTriangle />
+            Delete All Variables
+          </button>
         )}
       </div>
-      <div className="vars-label max-num-warn" style={{ display: `${warnStyle}` }}>Max {maxNumOfVars} Variables, delete some old vars to add more.</div>
+      <div
+        className="vars-label max-num-warn"
+        style={{ display: `${warnStyle}` }}
+      >
+        Max {maxNumOfVars} Variables, delete some old vars to add more.
+      </div>
     </>
   );
 };
 
 export default VariablesComponent;
-
